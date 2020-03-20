56ae0a49b10f32e81a1b4a9f115016216f985193
dome <jaypoopys@gmail.com>
2020-03-20 18:08:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -193,9 +193,7 @@ function submitDetails(checkout) {
 
           headers: {
             "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-            origin: "https://bdgastore.com",
-            referer: "https://bdgastore.com/"
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
           },
           jar: cookieJar,
           followAllRedirects: true,
